927fdb8fc67eee126c2558509255610a7d665610
Elias Villatoro <villatoroelias174@gmail.com>
2024-03-02 20:08:27 -0600

    primer_commit

---
+++ DO_WHILE_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main(){
+
+	int i = 0;
+	do{
+		cout<<i<<endl;
+		i++;
+	}while (i<=5);
+
+}
---
+++ WHILE 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main(){
+	int a = 5;
+	while(a>0){
+		cout<<a<<endl;
+		a--;
+	}
+	
+	cout<<endl;
+	int i = 0;
+	while(i<=5){
+		cout<<i<<endl;
+		i++;
+	}
+	
+	
+	
+	
+	system("Pause");
+}
